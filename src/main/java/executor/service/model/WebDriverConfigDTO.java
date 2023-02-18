ab1aa0b17c0ebf2f029e80df81df3726a835f433
EvheniyO <evgonop@gmail.com>
2023-02-18 13:40:59 +0200

    Create web driver config DTO

---
+++ src/main/java/executor/service/model/WebDriverConfigDTO.java
@@ -0,0 +1,63 @@
+package executor.service.model;
+
+import java.util.Objects;
+
+public class WebDriverConfigDTO {
+    private String webDriverExecutable;
+    private String userAgent;
+    private Long pageLoadTimeout;
+    private Long implicitlyWait;
+
+    public WebDriverConfigDTO(String webDriverExecutable, String userAgent, Long pageLoadTimeout, Long implicitlyWait) {
+        this.webDriverExecutable = webDriverExecutable;
+        this.userAgent = userAgent;
+        this.pageLoadTimeout = pageLoadTimeout;
+        this.implicitlyWait = implicitlyWait;
+    }
+
+    public String getWebDriverExecutable() {
+        return webDriverExecutable;
+    }
+
+    public void setWebDriverExecutable(String webDriverExecutable) {
+        this.webDriverExecutable = webDriverExecutable;
+    }
+
+    public String getUserAgent() {
+        return userAgent;
+    }
+
+    public void setUserAgent(String userAgent) {
+        this.userAgent = userAgent;
+    }
+
+    public Long getPageLoadTimeout() {
+        return pageLoadTimeout;
+    }
+
+    public void setPageLoadTimeout(Long pageLoadTimeout) {
+        this.pageLoadTimeout = pageLoadTimeout;
+    }
+
+    public Long getImplicitlyWait() {
+        return implicitlyWait;
+    }
+
+    public void setImplicitlyWait(Long implicitlyWait) {
+        this.implicitlyWait = implicitlyWait;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof WebDriverConfigDTO)) return false;
+        WebDriverConfigDTO that = (WebDriverConfigDTO) o;
+        return webDriverExecutable.equals(that.webDriverExecutable) && userAgent.equals(that.userAgent) &&
+                pageLoadTimeout.equals(that.pageLoadTimeout) && implicitlyWait.equals(that.implicitlyWait);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(webDriverExecutable, userAgent, pageLoadTimeout, implicitlyWait);
+    }
+}
