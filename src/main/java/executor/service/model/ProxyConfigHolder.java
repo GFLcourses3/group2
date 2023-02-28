8d7baa53fb1066030b201ffd9fbc2bb79d5fe640
Andrii Zhuvak <emandem757@gmail.com>
2023-02-28 15:14:40 +0200

    Added ProxyConfigHolder class. Added StepTest class.

---
+++ src/main/java/executor/service/model/ProxyConfigHolder.java
@@ -0,0 +1,45 @@
+package executor.service.model;
+
+import java.util.Objects;
+
+public class ProxyConfigHolder {
+    private ProxyNetworkConfig proxyNetworkConfig;
+    private ProxyCredentials proxyCredentials;
+
+    public ProxyConfigHolder() {
+    }
+
+    public ProxyConfigHolder(ProxyNetworkConfig proxyNetworkConfig, ProxyCredentials proxyCredentials) {
+        this.proxyNetworkConfig = proxyNetworkConfig;
+        this.proxyCredentials = proxyCredentials;
+    }
+
+    public ProxyNetworkConfig getProxyNetworkConfig() {
+        return proxyNetworkConfig;
+    }
+
+    public void setProxyNetworkConfig(ProxyNetworkConfig proxyNetworkConfig) {
+        this.proxyNetworkConfig = proxyNetworkConfig;
+    }
+
+    public ProxyCredentials getProxyCredentials() {
+        return proxyCredentials;
+    }
+
+    public void setProxyCredentials(ProxyCredentials proxyCredentials) {
+        this.proxyCredentials = proxyCredentials;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProxyConfigHolder that = (ProxyConfigHolder) o;
+        return Objects.equals(proxyNetworkConfig, that.proxyNetworkConfig) && Objects.equals(proxyCredentials, that.proxyCredentials);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(proxyNetworkConfig, proxyCredentials);
+    }
+}
