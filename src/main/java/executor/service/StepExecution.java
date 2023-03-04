bb7a878ae3b999dda484f68e944f1d26c986fc0f
Andrii Zhuvak <emandem757@gmail.com>
2023-03-04 19:36:01 +0200

    updated pom.xml added Selenium dependency. Added StepExecution interface. Added StepExecutionClickCSS class

---
+++ src/main/java/executor/service/StepExecution.java
@@ -0,0 +1,9 @@
+package executor.service;
+
+import executor.service.model.Step;
+import org.openqa.selenium.WebDriver;
+
+public interface StepExecution {
+    String getStepAction();
+    void step(WebDriver webDriver, Step step);
+}
