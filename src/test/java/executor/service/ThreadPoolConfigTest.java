959306ed20da7e15163a00487b5885cc24ca4440
Oleh Denkovych <74234001+DenkovychOleh@users.noreply.github.com>
2023-03-06 00:21:08 +0200

    Update ThreadPoolConfigTest.java

--- src/test/java/executor/service/ThreadPoolConfigTest.java
+++ src/test/java/executor/service/ThreadPoolConfigTest.java
@@ -3,7 +3,6 @@ package executor.service;
 import executor.service.model.ThreadPoolConfig;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ThreadPoolConfigTest {
